28648c4e37d6d9fc2606d3df12e720246417d3e7
Lucas Gangstad <last_talon@new.rr.com>
2020-12-12 22:51:13 -0600

    Add ArrayList (#25)
    Several methods from LinkedList have been moved to AbstractList because they
    can be used in many lists. The AbstractList method implementations will work,
    but may be naive or inefficient implementations depending on the particular
    concrete implementation. These can all be overridden in the concrete list
    classes that have implementation knowledge.

---
+++ lib/AbstractList.lua
@@ -0,0 +1,368 @@
+--- An partial list implementation.
+-- This partial implementation can be used to create concrete Lists. This
+-- implementation implements all Collection methods save Add and Remove. It
+-- doesn't implement Enumerable methods or List methods as it has no knowledge
+-- of the specific concrete implementation. Due to the lack of knowledge of the
+-- specific implementation method, some methods may be naive implementations
+-- for any particular implementation and can be overridden.
+--
+-- AbstractList has the same optional and required methods of Collection and
+-- List. AbstractList assumes all optional methods are intended to be
+-- implemented, however, they can be overridden if they should not in a
+-- particular concrete implementation.Instance.new("Accessory")
+--
+-- @version 0.1.0 2020-12-12
+-- @since 1.0
+
+local module = script.Parent
+local List = require(module:WaitForChild("List"))
+
+local ErrorOverride = "Abstract method %s must be overridden in first concrete subclass. Called directly from AbstractList."
+
+local AbstractList = List.new()
+
+AbstractList.__index = AbstractList
+
+--- Creates a new AbstractList abstract instance.
+--
+-- @return the new AbstractList
+function AbstractList.new()
+	local self = setmetatable({}, AbstractList)
+	return self
+end
+
+--- Creates an enumerator for the AbstractList.
+-- The enumerator can be used directly in a generic for loop similar to pairs
+-- or ipairs.
+--
+-- @return the enumerator generator
+-- @return the invariant state
+-- @return the control variable state
+function AbstractList:Enumerator()
+	error(string.format(ErrorOverride, "Enumerator"))
+end
+
+--- Determines whether the AbstractList contains an item.
+--
+-- @param item the item to locate in the AbstractList
+-- @return true if the item is in the AbstractList, false otherwise
+function AbstractList:Contains(item)
+	for _, value in self:Enumerator() do
+		if item == value then
+			return true
+		end
+	end
+	return false -- We're only sure its not there once we've checked everything
+end
+
+--- Determines whether the AbstractList contains multiple items.
+-- Checks for items in another Collection in the order they are enumerated.
+--
+-- @param items the Collection of items to locate in this AbstractList
+-- @return true if all items are in the AbstractList, false otherwise
+function AbstractList:ContainsAll(items)
+	for _, value in items:Enumerator() do
+		if not self:Contains(value) then
+			return false -- As soon as one is not contained all cannot be
+		end
+	end
+	return true -- We're only sure all are contained when we're done
+end
+
+--- Determines whether the AbstractList contains any of the provided items.
+-- Checks for items in another Collection in the order they are enumerated.
+--
+-- @param items the Collection of items to locate in this AbstractList
+-- @return true if any items are in the AbstractList, false otherwise
+function AbstractList:ContainsAny(items)
+	for _, value in items:Enumerator() do
+		if self:Contains(value) then
+			return true -- As soon as we find one we're good
+		end
+	end
+	return false -- We only know none are contained once we're done
+end
+
+--- Gets the number of items in the AbstractList.
+--
+-- @return the number of items
+function AbstractList:Count()
+	local count = 0
+	for _ in self:Enumerator() do
+		count = count + 1
+	end
+	return count
+end
+
+--- Determines whether the AbstractList has no elements.
+--
+-- @return true if the AbstractList empty, false otherwise
+function AbstractList:Empty()
+	return self:Count() == 0
+end
+
+--- Creates a new array indexed table of this AbstractList.
+-- The indices of the table are the same as the indices of the AbstractList, and
+-- all elements of the AbstractList are guaranteed to exist in the array indices
+-- of the table (all elements can be traversed with ipairs).
+--
+-- @return the array indexed table
+function AbstractList:ToArray()
+	local array = {}
+	for index, value in self:Enumerator() do
+		array[index] = value
+	end
+	return array
+end
+
+--- Creates a new table of this AbstractList.
+-- Lists use no indices that are not array indices, so this provides the
+-- same table as ToArray.
+--
+-- @return the table
+AbstractList.ToTable = AbstractList.ToArray -- They should be identical
+
+--- Adds an item to the AbstractList.
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param item the item to add
+-- @return true if the AbstractList changed as a result, false otherwise
+function AbstractList:Add()
+	error(string.format(ErrorOverride, "Add"))
+end
+
+--- Adds multiple items to the AbstractList.
+-- Adds items provided in another Collection in the order the Collection
+-- enumerates them.
+--
+-- @param items the Collection of items to add to this AbstractList
+-- @return true always since the AbstractList is always changed
+function AbstractList:AddAll(items)
+	for _, value in items:Enumerator() do
+		self:Add(value)
+	end
+	return true -- The AbstractList is always changed
+end
+
+--- Removes everything from the AbstractList.
+function AbstractList:Clear()
+	for i = self:Count(), 1, -1 do -- We go backward for less shuffling
+		self:Delete(i)
+	end
+end
+
+--- Removes the specified item from the AbstractList.
+-- Removes only a single item. If there are multiple of the same item, it
+-- removes only the first encountered in no guaranteed order. Shifts other
+-- elements to fill the gap left at the index of removal.
+--
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param item the item to remove from the AbstractList
+-- @return true if the AbstractList changed as a result, false otherwise
+function AbstractList:Remove()
+	error(string.format(ErrorOverride, "Remove"))
+end
+
+--- Removes all provided items from the AbstractList.
+-- Removes each instance of a provided item only once for each time provided.
+-- If there are multiple of the same item in this AbstractList, it removes only
+-- the first encountered when traversing the list from the first element to the
+-- last element each time one is provided.
+--
+-- @param items the Collection of items to remove from this AbstractList
+-- @return true if the AbstractList changed as a result, false otherwise
+function AbstractList:RemoveAll(items)
+	local changed = false -- We can't report a change immediately
+	for _, value in items:Enumerator() do
+		changed = self:Remove(value) or changed -- Changed now or before
+	end
+	return changed
+end
+
+--- Removes all items except those provided from the AbstractList.
+-- Retains only the items contained in the specified Collection regardless
+-- of duplicates. If there are duplicates they are all kept.
+--
+-- @param items the Collection of items to retain in this AbstractList
+-- @return true if the AbstractList changed as a result, false otherwise
+function AbstractList:RetainAll(items)
+	local changed = false -- We can't report a change immediately
+	for i = self:Count(), 1, -1 do -- We go backward for less shuffling
+		if not items:Contains(self:Get(i)) then -- If this isn't being retained
+			self:Delete(i)
+			changed = true
+		end
+	end
+	return changed
+end
+
+--- Gets the item at the beginning of the AbstractList.
+--
+-- @return the first item in the AbstractList
+-- @throw if the AbstractList is empty
+function AbstractList:First()
+	error(string.format(ErrorOverride, "First"))
+end
+
+--- Gets the item at the specified index in the AbstractList.
+--
+-- @param index the index to get
+-- @return the item in the AbstractList at the specified index
+-- @throw if the index is out of bounds of the AbstractList
+function AbstractList:Get()
+	error(string.format(ErrorOverride, "Get"))
+end
+
+--- Determines the index of the first occurrence of a specific item in the AbstractList.
+-- Starts from specified index or from the beginning if none is provided.
+--
+-- @param item the item to locate
+-- @param index the index to start looking from
+-- @return the index of the item in the AbstractList if found, 0 otherwise
+-- @throw if the index is out of bounds of the AbstractList
+function AbstractList:IndexOf()
+	error(string.format(ErrorOverride, "IndexOf"))
+end
+
+--- Gets the item at the end of the AbstractList.
+--
+-- @return the last item in the AbstractList
+-- @throw if the AbstractList is empty
+function AbstractList:Last()
+	error(string.format(ErrorOverride, "Last"))
+end
+
+--- Determines the index of the last occurrence of a specific item in the AbstractList.
+-- Only returns the very last occurrence of the item in the AbstractList.
+--
+-- @param item the item to locate
+-- @return the index of the item in the AbstractList if found, 0 otherwise
+function AbstractList:LastIndexOf()
+	error(string.format(ErrorOverride, "LastIndexOf"))
+end
+
+--- Creates a new sub-list of this AbstractList.
+-- Creates the list that is the portion of this AbstractList between the specified
+-- indices or from the first sepecified index to the end if only one index is
+-- specified.
+--
+-- @param first the index to start at
+-- @param last the index to end at
+-- @return the new AbstractList
+-- @throw if either index is out of bounds of the AbstractList
+function AbstractList:Sub()
+	error(string.format(ErrorOverride, "Sub"))
+end
+
+--- Removes the item from the AbstractList at the specified index.
+-- Shifts other elements to fill the gap left at the index of removal.
+--
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param index the index of the item to remove from the AbstractList
+-- @return true if the AbstractList changed as a result, false otherwise
+-- @throw if the index is out of bounds of the AbstractList
+function AbstractList:Delete()
+	error(string.format(ErrorOverride, "Delete"))
+end
+
+--- Inserts the item to the AbstractList at the specified index.
+-- Shifts other elements to make space at the index of insertion.
+--
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param index the index to insert the item in the AbstractList
+-- @param item the item to add
+-- @return true if the AbstractList changed as a result, false otherwise
+-- @throw if the index is out of bounds of the AbstractList
+function AbstractList:Insert()
+	error(string.format(ErrorOverride, "Insert"))
+end
+
+
+--- Inserts all items into the AbstractList at the specified index.
+-- Inserts all items from the provided Collection in no particular order.
+-- Shifts other elements to make space at the index of insertion.
+--
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param index the index to insert the items in the AbstractList
+-- @param items the Collection of items to add to this AbstractList
+-- @return true if the AbstractList changed as a result, false otherwise
+-- @throw if the index is out of bounds of the AbstractList
+function AbstractList:InsertAll()
+	error(string.format(ErrorOverride, "InsertAll"))
+end
+
+--- Gets an item from the end and removes that item from the AbstractList.
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @return the item in the AbstractList
+-- @throw if the AbstractList is empty
+function AbstractList:Pop()
+	error(string.format(ErrorOverride, "Pop"))
+end
+
+--- Adds an item to the end of the AbstractList.
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param item the item to add
+-- @return true if the AbstractList changed as a result, false otherwise
+function AbstractList:Push()
+	error(string.format(ErrorOverride, "Push"))
+end
+
+--- Sets the element at the specified index.
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param index the index to set
+-- @return true if the AbstractList changed as a result, false otherwise
+-- @throw if the index is out of bounds of the AbstractList
+function AbstractList:Set()
+	error(string.format(ErrorOverride, "Set"))
+end
+
+--- Gets an item from the beginning and removes that item from the AbstractList.
+-- Shifts other elements to fill the gap left.
+--
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @return the item in the AbstractList
+-- @throw if the AbstractList is empty
+function AbstractList:Shift()
+	error(string.format(ErrorOverride, "Shift"))
+end
+
+--- Adds an item to the beginning of the AbstractList.
+-- Shifts other elements to make space.
+--
+-- This method is optional. All AbstractList implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- @param item the item to add
+-- @return true if the AbstractList changed as a result, false otherwise
+function AbstractList:Unshift()
+	error(string.format(ErrorOverride, "Unshift"))
+end
+
+return AbstractList
---
+++ lib/ArrayList.lua
@@ -0,0 +1,379 @@
+--- An array based list of items.
+-- A List implementation that stores items using an array. This provides random
+-- access to any index, however does so at the expense of insertion and
+-- deletion except at the end of the list.
+--
+-- ArrayList implements all optional Collection methods as well as all
+-- optional List methods.
+--
+-- @version 0.1.0 2020-12-11
+-- @since 0.1
+
+local module = script.Parent
+local AbstractList = require(module:WaitForChild("AbstractList"))
+
+local ErrorConstruct = "Cannot construct ArrayList from type %s."
+local ErrorFirst = "No first element exists."
+local ErrorLast = "No last element exists."
+local ErrorBounds = "Index '%s' is out of bounds."
+local ErrorOrder = "Last index is smaller than first index."
+
+local ArrayList = AbstractList.new()
+
+ArrayList.__index = ArrayList
+
+--- Creates a new ArrayList.
+-- Creates a ArrayList copy of the provided collection or array indexed table
+-- if one is provided, otherwise creates an empty ArrayList.
+--
+-- @param collection the Collection or table to copy
+-- @return the new ArrayList
+function ArrayList.new(collection)
+	local self = setmetatable({}, ArrayList)
+	self.array = {}
+	if collection ~= nil then
+		local typeCollection = type(collection)
+		if typeCollection == "table" then
+			if type(collection.Enumerator) == "function" then
+				-- If there's an Enumerator, assume it acts as a collection
+				for _, value in collection:Enumerator() do
+					self.array[#self.array + 1] = value
+				end
+			else -- Otherwise its just a table (we can't know otherwise)
+				for _, value in ipairs(collection) do
+					self.array[#self.array + 1] = value
+				end
+			end
+		else
+			error(string.format(ErrorConstruct, typeCollection))
+		end
+	end
+	return self
+end
+
+--- Creates an enumerator for the ArrayList.
+-- The enumerator can be used directly in a generic for loop similar to pairs
+-- or ipairs.
+--
+-- @return the enumerator generator
+-- @return the invariant state
+-- @return the control variable state
+function ArrayList:Enumerator()
+	return ipairs(self.array)
+end
+
+--- Gets the number of items in the ArrayList.
+--
+-- @return the number of items
+function ArrayList:Count()
+	return #self.array
+end
+
+--- Removes everything from the ArrayList.
+function ArrayList:Clear()
+	self.array = {} -- The array will be garbage collected (may still be in use)
+end
+
+--- Removes the specified item from the ArrayList.
+-- Removes only a single item. If there are multiple of the same item, it
+-- removes only the first encountered when traversing the list from the first
+-- element to the last element. Shifts other elements to fill the gap left a
+-- the index of removal.
+--
+-- @param item the item to remove from the ArrayList
+-- @return true if the ArrayList changed as a result, false otherwise
+function ArrayList:Remove(item)
+	for index, value in ipairs(self.array) do
+		if value == item then
+			table.remove(self.array, index)
+			return true -- The ArrayList has been changed
+		end
+	end
+	return false -- Nothing changed
+end
+
+--- Removes all provided items from the ArrayList.
+-- Removes each instance of a provided item only once for each time provided.
+-- If there are multiple of the same item in this ArrayList, it removes only
+-- the first encountered when traversing the list from the first element to the
+-- last element each time one is provided.
+--
+-- @param items the Collection of items to remove from this ArrayList
+-- @return true if the ArrayList changed as a result, false otherwise
+function ArrayList:RemoveAll(items)
+	local remove = {}
+	for _, value in items:Enumerator() do
+		remove[value] = (remove[value] or 0) + 1
+	end
+	local array = self.array
+	local changed = false -- We can't report a change immediately
+	local empty = 1
+	for index, value in ipairs(array) do
+		if not remove[value] or remove[value] == 0 then -- If we aren't removing it
+			if index ~= empty then -- If we're not at the earlier empty index
+				array[empty] = value -- Move our value there
+				array[index] = nil
+			end
+			empty = empty + 1
+		else -- If this is being removed
+			remove[value] = remove[value] - 1 -- Remove it from our multiset
+			array[index] = nil
+			changed = true
+		end
+	end
+	return changed
+end
+
+--- Removes all items except those provided from the ArrayList.
+-- Retains only the items contained in the specified Collection regardless
+-- of duplicates. If there are duplicates they are all kept.
+--
+-- @param items the Collection of items to retain in this ArrayList
+-- @return true if the ArrayList changed as a result, false otherwise
+function ArrayList:RetainAll(items)
+	local array = self.array
+	local changed = false -- We can't report a change immediately
+	local empty = 1
+	for index, value in ipairs(array) do
+		if items:Contains(value) then -- If this is being retained
+			if index ~= empty then -- If we're not at the earlier empty index
+				array[empty] = value -- Move our value there
+				array[index] = nil
+			end
+			empty = empty + 1
+		else -- If this isn't being retained
+			array[index] = nil
+			changed = true
+		end
+	end
+	return changed
+end
+
+--- Gets the item at the beginning of the ArrayList.
+--
+-- @return the first item in the ArrayList
+-- @throw if the ArrayList is empty
+function ArrayList:First()
+	if #self.array == 0 then
+		error(ErrorFirst)
+	end
+	return self.array[1]
+end
+
+--- Gets the item at the specified index in the ArrayList.
+--
+-- @param index the index to get
+-- @return the item in the ArrayList at the specified index
+-- @throw if the index is out of bounds of the ArrayList
+function ArrayList:Get(index)
+	if index < 1 or index > #self.array then
+		error(string.format(ErrorBounds, index))
+	end
+	return self.array[index]
+end
+
+--- Determines the index of the first occurrence of an item in the ArrayList.
+-- Starts from specified index or from the beginning if none is provided.
+--
+-- @param item the item to locate
+-- @param index the index to start looking from
+-- @return the index of the item in the ArrayList if found, 0 otherwise
+-- @throw if the index is out of bounds of the ArrayList
+function ArrayList:IndexOf(item, index)
+	local array = self.array
+	if index == nil then
+		index = 1 -- If index isn't provided, start at the beginning
+	elseif index < 1 or index > #array then -- If it is, check the bounds
+		error(string.format(ErrorBounds, index))
+	end
+	if #array == 0 then
+		return 0 -- If we're empty, its not here
+	end
+	for currentIndex = index, #array do
+		if array[currentIndex] == item then
+			return currentIndex
+		end
+	end
+	return 0 -- It wasn't here
+end
+
+--- Gets the item at the end of the ArrayList.
+--
+-- @return the last item in the ArrayList
+-- @throw if the ArrayList is empty
+function ArrayList:Last()
+	if #self.array == 0 then
+		error(ErrorLast)
+	end
+	return self.array[#self.array]
+end
+
+--- Determines the index of the last occurrence of an item in the ArrayList.
+-- Only returns the very last occurrence of the item in the ArrayList.
+--
+-- @param item the item to locate
+-- @return the index of the item in the ArrayList if found, 0 otherwise
+function ArrayList:LastIndexOf(item)
+	local array = self.array
+	if #array == 0 then
+		return 0 -- If we're empty, its not here
+	end
+	for index = #array, 1, -1 do
+		if array[index] == item then
+			return index
+		end
+	end
+	return 0 -- It wasn't here
+end
+
+--- Creates a new sub-list of this ArrayList.
+-- Creates the list that is the portion of this ArrayList between the
+-- specified indices or from the first specified index to the end if only one
+-- index is specified.
+--
+-- @param first the index to start at
+-- @param last the index to end at
+-- @return the new ArrayList
+-- @throw if the first index is out of bounds
+-- @throw if the last index is out of bounds
+-- @throw if the last index is smaller than the first index
+function ArrayList:Sub(first, last)
+	local array = self.array
+	last = last or #array -- If last isn't provided, go to the end
+	if first < 1 or first > #array then
+		error(string.format(ErrorBounds, first))
+	end
+	if last < 1 or last > #array then
+		error(string.format(ErrorBounds, last))
+	end
+	if last < first then
+		error(ErrorOrder)
+	end
+	local sub = ArrayList.new()
+	for index = first, last do
+			sub:Push(array[index])
+	end
+	return sub
+end
+
+--- Removes the item from the ArrayList at the specified index.
+-- Shifts other elements to fill the gap left at the index of removal.
+--
+-- @param index the index of the item to remove from the ArrayList
+-- @return true always since the ArrayList is always changed
+-- @throw if the index is out of bounds of the ArrayList
+function ArrayList:Delete(index)
+	if index < 1 or index > #self.array then
+		error(string.format(ErrorBounds, index))
+	end
+	table.remove(self.array, index)
+	return true -- The ArrayList is always changed
+end
+
+--- Inserts the item to the ArrayList at the specified index.
+-- Shifts other elements to make space at the index of insertion.
+--
+-- @param index the index to insert the item in the ArrayList
+-- @param item the item to add
+-- @return true always since the ArrayList is always changed
+-- @throw if the index is out of bounds of the ArrayList
+function ArrayList:Insert(index, item)
+	if index < 1 or index > #self.array + 1 then
+		error(string.format(ErrorBounds, index))
+	end
+	table.insert(self.array, index, item)
+	return true -- The ArrayList is always changed
+end
+
+--- Inserts multiple items into the ArrayList at the specified index.
+-- Inserts all items from the provided Collection in the order they are
+-- enumerated. Shifts other elements to make space at the index of insertion.
+--
+-- @param index the index to insert the items in the ArrayList
+-- @param items the Collection of items to add to this ArrayList
+-- @return true always since the ArrayList is always changed
+-- @throw if the index is out of bounds of the ArrayList
+function ArrayList:InsertAll(index, items)
+	local array = self.array
+	if index < 1 or index > #array + 1 then
+		error(string.format(ErrorBounds, index))
+	end
+	local offset = items:Count()
+	for currentIndex = #array, index, -1 do
+		array[currentIndex + offset] = array[currentIndex]
+	end
+	for _, value in items:Enumerator() do
+		array[index] = value
+		index = index + 1
+	end
+	return true
+end
+
+--- Gets an item from the end and removes that item from the ArrayList.
+--
+-- @return the item in the ArrayList
+-- @throw if the ArrayList is empty
+function ArrayList:Pop()
+	if #self.array == 0 then
+		error(ErrorLast)
+	end
+	local last = self.array[#self.array]
+	self.array[#self.array] = nil
+	return last
+end
+
+--- Adds an item to the end of the ArrayList.
+--
+-- @param item the item to add
+-- @return true always since the ArrayList is always changed
+function ArrayList:Push(item)
+	self.array[#self.array + 1] = item
+	return true -- The ArrayList is always changed
+end
+
+--- Adds an item to the ArrayList.
+--
+-- @param item the item to add
+-- @return true always since the ArrayList is always changed
+ArrayList.Add = ArrayList.Push
+
+--- Sets the element at the specified index.
+--
+-- @param index the index to set
+-- @param	item the item to set at the index
+-- @return true if the ArrayList changed as a result, false otherwise
+-- @throw if the index is out of bounds of the ArrayList
+function ArrayList:Set(index, item)
+	if index < 1 or index > #self.array then
+		error(string.format(ErrorBounds, index))
+	end
+	local same = self.array[index] == item
+	self.array[index] = item
+	return not same
+end
+
+--- Gets an item from the beginning and removes that item from the ArrayList.
+-- Shifts other elements to fill the gap left.
+--
+-- @return the item in the ArrayList
+-- @throw if the ArrayList is empty
+function ArrayList:Shift()
+	if #self.array == 0 then
+		error(ErrorFirst)
+	end
+	local first = self.array[1]
+	table.remove(self.array, 1)
+	return first
+end
+
+--- Adds an item to the beginning of the ArrayList.
+-- Shifts other elements to make space.
+--
+-- @param item the item to add
+-- @return true always since the ArrayList is always changed
+function ArrayList:Unshift(item)
+	table.insert(self.array, 1, item)
+	return true -- The ArrayList is always changed
+end
+
+return ArrayList
