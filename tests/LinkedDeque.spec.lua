5964b650e6875dee5194a133625adc32e7878dd1
Lucas Gangstad <last_talon@new.rr.com>
2021-03-03 19:36:40 -0600

    Update style with StyLua

--- tests/LinkedDeque.spec.lua
+++ tests/LinkedDeque.spec.lua
@@ -127,14 +127,11 @@ return function()
 					expect(deque:ContainsAll(contained)).to.equal(false)
 				end)
 
-				it(
-					"should find all 0 elements when the provided collection is empty",
-					function()
-						local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
-						local contained = LinkedDeque.new()
-						expect(deque:ContainsAll(contained)).to.equal(true)
-					end
-				)
+				it("should find all 0 elements when the provided collection is empty", function()
+					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
+					local contained = LinkedDeque.new()
+					expect(deque:ContainsAll(contained)).to.equal(true)
+				end)
 
 				it("should find all elements when the elements exist", function()
 					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
@@ -746,14 +743,11 @@ return function()
 					expect(deque:Count()).to.equal(0)
 				end)
 
-				it(
-					"should return true when not retaining a single element which does not match",
-					function()
-						local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
-						local retain = LinkedDeque.new({ 3 })
-						expect(deque:RetainAll(retain)).to.equal(true)
-					end
-				)
+				it("should return true when not retaining a single element which does not match", function()
+					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
+					local retain = LinkedDeque.new({ 3 })
+					expect(deque:RetainAll(retain)).to.equal(true)
+				end)
 
 				it("should retain multiple elements when more exist", function()
 					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
@@ -762,24 +756,18 @@ return function()
 					expect(deque:Count()).to.equal(3)
 				end)
 
-				it(
-					"should return true when retaining multiple elements when more exist",
-					function()
-						local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
-						local retain = LinkedDeque.new({ 1, 3, 5 })
-						expect(deque:RetainAll(retain)).to.equal(true)
-					end
-				)
+				it("should return true when retaining multiple elements when more exist", function()
+					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
+					local retain = LinkedDeque.new({ 1, 3, 5 })
+					expect(deque:RetainAll(retain)).to.equal(true)
+				end)
 
-				it(
-					"should retain multiple elements when attempting to retain excess",
-					function()
-						local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
-						local retain = LinkedDeque.new({ 0, 1, 3 })
-						deque:RetainAll(retain)
-						expect(deque:Count()).to.equal(2)
-					end
-				)
+				it("should retain multiple elements when attempting to retain excess", function()
+					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
+					local retain = LinkedDeque.new({ 0, 1, 3 })
+					deque:RetainAll(retain)
+					expect(deque:Count()).to.equal(2)
+				end)
 
 				it("should return true when attemping to retain with excess", function()
 					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
@@ -807,14 +795,11 @@ return function()
 					expect(deque:Count()).to.equal(5)
 				end)
 
-				it(
-					"should return false when attempting to retain exceess and retaining all elements",
-					function()
-						local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
-						local retain = LinkedDeque.new({ 0, 1, 2, 3, 4, 5 })
-						expect(deque:RetainAll(retain)).to.equal(false)
-					end
-				)
+				it("should return false when attempting to retain exceess and retaining all elements", function()
+					local deque = LinkedDeque.new({ 1, 2, 3, 4, 5 })
+					local retain = LinkedDeque.new({ 0, 1, 2, 3, 4, 5 })
+					expect(deque:RetainAll(retain)).to.equal(false)
+				end)
 
 				it("should retain all duplicates", function()
 					local deque = LinkedDeque.new({ 1, 1, 2, 2, 3 })
