96b03c5ee2ff5389c6a8e59c9f929f3ae4288094
Lucas Gangstad <last_talon@new.rr.com>
2021-03-24 15:15:50 -0500

    Add Set docs

--- lib/Set.lua
+++ lib/Set.lua
@@ -1,3 +1,27 @@
+--- An unordered @{Collection} of unique items.
+-- Sets provide no particular ordering of their items, only either containing
+-- or not containing elements. Any particular concrete Set implementation may
+-- have an internal ordering, but this ordering does not affect the operation
+-- of the Set.
+--
+-- The Set interface provides a base set of operations for interacting
+-- with any abstract Set type. Abstract data types may provide addtional
+-- specific operations based on the particular implemented type. Concrete
+-- implementations, such as @{HashSet|HashSets} ultimately determine the
+-- properties of the concrete Set such as time and space complexity for any
+-- operations.
+--
+-- The Set interface provides certain optional methods in addition to those in
+-- @{Collection}. Some Sets, such as immutable or type-restricted data types,
+-- may not be able to provide full functionality for these methods. All Sets
+-- are guaranteed to provide a required set of behaviors without exception and,
+-- unless otherwise noted, a method is required. All Sets should attempt
+-- to provide optional functionality, if they're able, regardless.
+--
+-- **Implements:** @{Collection}, @{Enumerable}
+--
+-- @classmod Set
+
 local module = script.Parent
 local Collection = require(module:WaitForChild("Collection"))
 
@@ -199,42 +223,120 @@ function Set:RetainAll()
 	error(string.format(ErrorOverride, "RetainAll"))
 end
 
+--- Determines whether the Set elements overlaps a @{Collection}.
+--
+-- @param items the @{Collection} of items to locate in this Set
+-- @return true if they overlap, false otherwise
 function Set:Overlaps()
 	error(string.format(ErrorOverride, "Overlaps"))
 end
 
+--- Determines whether the Set is a strict subset of a @{Collection}.
+-- A proper or strict subset is one where every element in this set is
+-- contained in the other, and the other contains at least one additional
+-- element.
+--
+-- @param items the @{Collection} of items to check against
+-- @return true if the Set is a proper subset, false otherwise
 function Set:ProperSubsetOf()
 	error(string.format(ErrorOverride, "ProperSubsetOf"))
 end
 
+--- Determines whether the Set is a strict superset of a @{Collection}.
+-- A proper or strict superset is one where every element in the other is
+-- contained in this set, and this set contains at least one additional
+-- element.
+--
+-- @param items the @{Collection} of items to check against
+-- @return true if the Set is a proper superset, false otherwise
 function Set:ProperSupersetOf()
 	error(string.format(ErrorOverride, "ProperSupersetOf"))
 end
 
+--- Determines whether the Set is a subset of a @{Collection}.
+-- A subset is one where every element in this Set is contained in the other.
+--
+-- @param items the @{Collection} of items to check against
+-- @return true if the Set is a superset, false otherwise
 function Set:SubsetOf()
 	error(string.format(ErrorOverride, "SubsetOf"))
 end
 
+--- Determines whether the Set is a superset of a @{Collection}.
+-- A superset is one where every element in the other is contained in this set.
+--
+-- @param items the @{Collection} of items to check against
+-- @return true if the Set is a superset, false otherwise
 function Set:SupersetOf()
 	error(string.format(ErrorOverride, "SupersetOf"))
 end
 
+--- Determines whether the Set contains the same elements as a @{Collection}.
+-- Determines only that the same exact Sets of elements are contained and does
+-- not care for duplicates or any other associated data.
+--
+-- @param items the @{Collection} of items to check against
+-- @return true if the Sets are equal, false otherwise
 function Set:SetEquals()
 	error(string.format(ErrorOverride, "SetEquals"))
 end
 
+--- Transforms the Set to remove all elements from a @{Collection}.
+--
+-- This method is optional. All Set implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- This method should always be overridden regardless of implementation. If
+-- unimplemented, it should return an error specific to the optional
+-- functionality that can't be provided by this Set.
+--
+-- @param items the @{Collection} of items to except
 function Set:Except()
 	error(string.format(ErrorOverride, "Except"))
 end
 
+--- Transforms the Set to keep only elements from a @{Collection}.
+--
+-- This method is optional. All Set implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- This method should always be overridden regardless of implementation. If
+-- unimplemented, it should return an error specific to the optional
+-- functionality that can't be provided by this Set.
+--
+-- @param items the @{Collection} of items to intersect
 function Set:Intersect()
 	error(string.format(ErrorOverride, "Intersect"))
 end
 
+--- Transforms the Set to keep only elements in it or a @{Collection}, not both.
+--
+-- This method is optional. All Set implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- This method should always be overridden regardless of implementation. If
+-- unimplemented, it should return an error specific to the optional
+-- functionality that can't be provided by this Set.
+--
+-- @param items the @{Collection} of items to symmetric except
 function Set:SymmetricExcept()
 	error(string.format(ErrorOverride, "SymmetricExcept"))
 end
 
+--- Transforms the Set to include all elements from a @{Collection}.
+--
+-- This method is optional. All Set implementations should attempt to
+-- implement this method, but some may be unable to do so or may need to
+-- impose additional conditions to do so.
+--
+-- This method should always be overridden regardless of implementation. If
+-- unimplemented, it should return an error specific to the optional
+-- functionality that can't be provided by this Set.
+--
+-- @param items the @{Collection} of items to union
 function Set:Union()
 	error(string.format(ErrorOverride, "Union"))
 end
